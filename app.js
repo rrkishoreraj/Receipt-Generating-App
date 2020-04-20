2ff15b43366e05bb600f39f789b70b2271cc087a
Kishore Karthikaeyan <kishore.r@codingmart.com>
2020-04-20 20:29:46 +0530

    Added logout button and resolved small bugs

--- app.js
+++ app.js
@@ -20,7 +20,13 @@ app.use(bodyParser.json());
 app.set("view engine", "html");
 app.use(express.static(__dirname + "/public"));
 
-let userMail;
+let user, userMail, password;
+
+const authenticate = () => {
+  return user === "admin" && userMail === "admin@ad.com" && password === "admin"
+    ? true
+    : false;
+};
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/views" + "/index.html"));
@@ -29,11 +35,13 @@ app.get("/", (req, res) => {
 app.post("/login", (req, res) => {
   console.log("Login");
   if (
-    req.body.name == "admin" &&
-    req.body.email == "admin@ad.com" &&
-    req.body.password == "admin"
+    req.body.name === "admin" &&
+    req.body.email === "admin@ad.com" &&
+    req.body.password === "admin"
   ) {
+    user = req.body.name;
     userMail = req.body.email;
+    password = req.body.password;
     res.redirect("/receipt");
   } else {
     res.send("Invalid Credentials Entered");
@@ -41,107 +49,140 @@ app.post("/login", (req, res) => {
   //res.send("Nodemon auto updates app.js when I save this file!...Cooool!!!")
 });
 
+app.get("/logout", (req, res) => {
+  (user = ""), (userMail = ""), (password = "");
+  res.redirect("/");
+});
+
 app.get("/receipt", (req, res) => {
-  res.sendFile(path.join(__dirname + "/views" + "/receipt.html"));
+  if (authenticate()) {
+    res.sendFile(path.join(__dirname + "/views" + "/receipt.html"));
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.post("/generateForm", (req, res) => {
-  var dbmodel = mongoose.model("vendordetails", schema);
-  var newdbModel = new dbmodel();
-  newdbModel.custName = req.body.custName;
-  newdbModel.custId = req.body.custId;
-  newdbModel.custMail = req.body.custMail;
-  newdbModel.custPhno = req.body.custPhno;
-  newdbModel.custAddr = req.body.custAddr;
-  newdbModel.invoiceNo = req.body.invoiceNo;
-  newdbModel.invoiceCreated = req.body.invoiceCreated;
-  newdbModel.paidOn = req.body.paidOn;
-  newdbModel.paymentMode = req.body.paymentMode;
-  newdbModel.noOfItems = req.body.noOfItems;
+  if (authenticate()) {
+    var dbmodel = mongoose.model("vendordetails", schema);
+    var newdbModel = new dbmodel();
+    newdbModel.custName = req.body.custName;
+    newdbModel.custId = req.body.custId;
+    newdbModel.custMail = req.body.custMail;
+    newdbModel.custPhno = req.body.custPhno;
+    newdbModel.custAddr = req.body.custAddr;
+    newdbModel.invoiceNo = req.body.invoiceNo;
+    newdbModel.invoiceCreated = req.body.invoiceCreated;
+    newdbModel.paidOn = req.body.paidOn;
+    newdbModel.paymentMode = req.body.paymentMode;
+    newdbModel.noOfItems = req.body.noOfItems;
 
-  let purchasedItems = [];
-  for (let i = 0; i < req.body.noOfItems; i++) {
-    purchasedItems.push({
-      item: req.body.item[i],
-      quantity: Number(req.body.quantity[i]),
-      unitPrice: parseFloat(req.body.unitPrice[i]),
-      total: parseFloat(req.body.total[i]),
-    });
+    let purchasedItems = [];
+    for (let i = 0; i < req.body.noOfItems; i++) {
+      purchasedItems.push({
+        item: req.body.item[i],
+        quantity: Number(req.body.quantity[i]),
+        unitPrice: parseFloat(req.body.unitPrice[i]),
+        total: parseFloat(req.body.total[i]),
+      });
+    }
+    newdbModel.purchasedItems.push(purchasedItems);
+
+    dbmodel
+      .find({}, (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(result);
+          !result[0]
+            ? (newdbModel.receiptNo = 1000)
+            : (newdbModel.receiptNo = result[0].receiptNo + 1);
+          newdbModel.save((err, saved) => {
+            if (saved) {
+              console.log("Document Saved Successfully!");
+              res.redirect("/generatedReceipt");
+            } else {
+              console.log(err);
+              res.send(err);
+            }
+          });
+        }
+      })
+      .sort({ _id: -1 })
+      .limit(1);
+  } else {
+    res.redirect("/");
   }
-  newdbModel.purchasedItems.push(purchasedItems);
-
-  dbmodel
-    .find({}, (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(result);
-        !result[0]
-          ? (newdbModel.receiptNo = 1000)
-          : (newdbModel.receiptNo = result[0].receiptNo + 1);
-        newdbModel.save((err, saved) => {
-          if (saved) {
-            console.log("Document Saved Successfully!");
-            res.redirect("/generatedReceipt");
-          } else {
-            console.log(err);
-            res.send(err);
-          }
-        });
-      }
-    })
-    .sort({ _id: -1 })
-    .limit(1);
 });
 
 app.get("/generatedReceipt", (req, res) => {
-  res.sendFile(path.join(__dirname + "/views" + "/receiptTemplate.html"));
+  if (authenticate()) {
+    res.sendFile(path.join(__dirname + "/views" + "/receiptTemplate.html"));
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.get("/getReceiptDetail", (req, res) => {
-  var dbmodel = mongoose.model("vendordetails", schema);
+  if (authenticate()) {
+    var dbmodel = mongoose.model("vendordetails", schema);
 
-  dbmodel
-    .find({}, (err, result) => {
+    dbmodel
+      .find({}, (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(result);
+          res.send(result);
+        }
+      })
+      .sort({ _id: -1 })
+      .limit(1);
+  } else {
+    res.redirect("/");
+  }
+});
+
+app.post("/viewReceipt", (req, res) => {
+  if (authenticate()) {
+    var dbmodel = mongoose.model("vendordetails", schema);
+    let id = req.body.id;
+
+    dbmodel.find({ receiptNo: id }, (err, result) => {
       if (err) {
         console.log(err);
       } else {
         console.log(result);
         res.send(result);
       }
-    })
-    .sort({ _id: -1 })
-    .limit(1);
-});
-
-app.post("/viewReceipt", (req, res) => {
-  var dbmodel = mongoose.model("vendordetails", schema);
-  let id = req.body.id;
-
-  dbmodel.find({ receiptNo: id }, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(result);
-      res.send(result);
-    }
-  });
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.get("/allReceipts", (req, res) => {
-  res.sendFile(path.join(__dirname + "/views" + "/allreceipts.html"));
+  if (authenticate()) {
+    res.sendFile(path.join(__dirname + "/views" + "/allreceipts.html"));
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.get("/getAllReceipts", (req, res) => {
-  var dbmodel = mongoose.model("vendordetails", schema);
+  if (authenticate()) {
+    var dbmodel = mongoose.model("vendordetails", schema);
 
-  dbmodel.find({}, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.status(200).send(result);
-    }
-  });
+    dbmodel.find({}, (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.status(200).send(result);
+      }
+    });
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.listen(5000, () => {
